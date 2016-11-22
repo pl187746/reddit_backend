b7d8885401c8ed07f752c599821cc40d5569dbb7
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-22 21:21:35 +0100

    Ladowanie newsow spod url. (wciaz fake)

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/RedditWorker.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/RedditWorker.java
@@ -1,7 +1,9 @@
 package pl.lodz.p.iis.ppkwu.reddit.backend;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 
 import pl.lodz.p.iis.ppkwu.reddit.api.Callback;
@@ -9,6 +11,7 @@ import pl.lodz.p.iis.ppkwu.reddit.api.Category;
 import pl.lodz.p.iis.ppkwu.reddit.api.News;
 import pl.lodz.p.iis.ppkwu.reddit.api.Page;
 import pl.lodz.p.iis.ppkwu.reddit.api.Result;
+import pl.lodz.p.iis.ppkwu.reddit.api.ResultStatus;
 import pl.lodz.p.iis.ppkwu.reddit.api.Subreddit;
 import pl.lodz.p.iis.ppkwu.reddit.api.User;
 import pl.lodz.p.iis.ppkwu.reddit.backend.data.builders.PageBuilder;
@@ -32,14 +35,33 @@ public class RedditWorker {
 	}
 
 	public void loadSubredditNews(Subreddit subreddit, Category category, Callback<Page<News>> callback) {
-		fakeEmptyPage(callback);
+		Callable<URL> callUrl = () -> UrlGenerator.subredditNewsUrl(subreddit, category);
+		loadNewsFromUrl(callUrl, callback);
 	}
 
 	public void loadUserNews(User user, Callback<Page<News>> callback) {
-		fakeEmptyPage(callback);
+		Callable<URL> callUrl = () -> UrlGenerator.userNewsUrl(user);
+		loadNewsFromUrl(callUrl, callback);
 	}
 
 	public void loadNewsByKeywords(List<String> keywords, Callback<Page<News>> callback) {
+		Callable<URL> callUrl = () -> UrlGenerator.newsByKeywordsUrl(keywords);
+		loadNewsFromUrl(callUrl, callback);
+	}
+
+	public void loadNewsFromUrl(Callable<URL> callUrl, Callback<Page<News>> callback) {
+		try {
+			URL url = callUrl.call();
+			loadNewsFromUrl(url, callback);
+		} catch (Exception e) {
+			Result<Page<News>> result = new ResultBuilder<Page<News>>()
+				.withStatus(ResultStatus.CONNECTION_ERROR)
+				.build();
+			runCallback(callback, result);
+		}
+	}
+
+	public void loadNewsFromUrl(URL url, Callback<Page<News>> callback) {
 		fakeEmptyPage(callback);
 	}
 
