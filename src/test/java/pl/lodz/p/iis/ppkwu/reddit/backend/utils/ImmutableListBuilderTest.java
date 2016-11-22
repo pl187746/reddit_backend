6f6dd3465f275d9557eaacfeaf4963e85d1ad25f
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-22 16:55:31 +0100

    testy ImmutableListBuilder

---
+++ src/test/java/pl/lodz/p/iis/ppkwu/reddit/backend/utils/ImmutableListBuilderTest.java
@@ -0,0 +1,54 @@
+package pl.lodz.p.iis.ppkwu.reddit.backend.utils;
+
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class ImmutableListBuilderTest {
+
+	@Test
+	public void domyslnieBudowanaPustaLista() {
+		ImmutableListBuilder<Integer> builder = new ImmutableListBuilder<>();
+		List<Integer> list = builder.build();
+		assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void wynikowaListaZawieraDodaneElementy() {
+		ImmutableListBuilder<Integer> builder = builderWithInts0to4();
+		List<Integer> list = builder.build();
+		assertThat(list, contains(0, 1, 2, 3, 4));
+	}
+
+	@Test
+	public void modyfikacjaZrodlowejListyNieZmieniaWyniku() {
+		ImmutableListBuilder<Integer> builder = builderWithInts0to4();
+		List<Integer> list = builder.build();
+		for (int i = 10; i < 15; ++i) {
+			builder.addEntry(i);
+		}
+		assertThat(list, contains(0, 1, 2, 3, 4));
+		builder.editableList().remove(2);
+		assertThat(list, contains(0, 1, 2, 3, 4));
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void wynikowaListaJestNiemodyfikowalna() {
+		ImmutableListBuilder<Integer> builder = builderWithInts0to4();
+		List<Integer> list = builder.build();
+		list.set(0, 1);
+	}
+
+	private ImmutableListBuilder<Integer> builderWithInts0to4() {
+		ImmutableListBuilder<Integer> builder = new ImmutableListBuilder<>();
+		for (int i = 0; i < 5; ++i) {
+			builder.addEntry(i);
+		}
+		return builder;
+	}
+
+}
