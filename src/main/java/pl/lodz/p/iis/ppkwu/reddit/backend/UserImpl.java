547021ce17eb21386344fb6a908cb4f396a5a3f9
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-22 12:08:37 +0100

    Klasa UserImpl

---
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/UserImpl.java
@@ -0,0 +1,18 @@
+package pl.lodz.p.iis.ppkwu.reddit.backend;
+
+import pl.lodz.p.iis.ppkwu.reddit.api.User;
+
+public class UserImpl implements User {
+	
+	private String login;
+
+	@Override
+	public String login() {
+		return login;
+	}
+	
+	public UserImpl(String login) {
+		this.login = login;
+	}
+
+}
