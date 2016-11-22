9b4b5aedfc0ad63bbf4ca182e46609c9f98dc631
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-22 14:43:05 +0100

    Kategoria zawiera relatywny url

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/data/CategoryImpl.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/data/CategoryImpl.java
@@ -5,14 +5,20 @@ import pl.lodz.p.iis.ppkwu.reddit.api.Category;
 public class CategoryImpl implements Category {
 
 	private final String name;
+	private final String relativeUrl;
 
-	public CategoryImpl(String name) {
+	public CategoryImpl(String name, String relativeUrl) {
 		this.name = name;
+		this.relativeUrl = relativeUrl;
 	}
 
 	@Override
 	public String name() {
 		return name;
+	}
+
+	public String relativeUrl() {
+		return relativeUrl;
 	}
 
 }
