74dd449348980bdefc6a066e68478252cbe783c4
Michal Warcholinski <187834@CARBON.CTI.P.LODZ.PL>
2016-11-22 11:21:32 +0100

    Implementacja klasy Category

---
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryImpl.java
@@ -0,0 +1,20 @@
+package pl.lodz.p.iis.ppkwu.reddit.backend;
+
+import pl.lodz.p.iis.ppkwu.reddit.api.Category;
+
+public class CategoryImpl implements Category {
+
+	private String name;
+
+	public CategoryImpl(String name) {
+		this.name = name;
+
+	}
+
+	@Override
+	public String name() {
+
+		return name;
+	}
+
+}
