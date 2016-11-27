b36249d8d215557cb96ba3d772f06a88c3357829
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-27 18:41:32 +0100

    Niepoprawne foramtowanie automatycznie podmienia spacje na taby więc zmodyfikował cały plik

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/NewsLoader.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/NewsLoader.java
@@ -18,52 +18,52 @@ import pl.lodz.p.iis.ppkwu.reddit.backend.utils.Downloader;
 
 public class NewsLoader {
 
-	private URL url;
+    private URL url;
 
-	private PageBuilder<News> pageBuilder = new PageBuilder<>();
+    private PageBuilder<News> pageBuilder = new PageBuilder<>();
 
-	private ResultStatus resultStatus = ResultStatus.SUCCEEDED;
+    private ResultStatus resultStatus = ResultStatus.SUCCEEDED;
 
-	public NewsLoader(URL url) {
-		this.url = url;
-	}
+    public NewsLoader(URL url) {
+        this.url = url;
+    }
 
-	public Result<Page<News>> loadNews() {
-		try {
-			perform();
-		} catch (StatusException se) {
-			resultStatus = se.getStatus();
-		}
-		return getResult();
-	}
+    public Result<Page<News>> loadNews() {
+        try {
+            perform();
+        } catch (StatusException se) {
+            resultStatus = se.getStatus();
+        }
+        return getResult();
+    }
 
-	public Result<Page<News>> getResult() {
-		return new ResultBuilder<Page<News>>().withContent(pageBuilder.build()).withStatus(resultStatus).build();
-	}
+    public Result<Page<News>> getResult() {
+        return new ResultBuilder<Page<News>>().withContent(pageBuilder.build()).withStatus(resultStatus).build();
+    }
 
-	private void perform() throws StatusException {
-		byte[] data = download();
-		Document document = parse(data);
-		extractNews(document);
-	}
+    private void perform() throws StatusException {
+        byte[] data = download();
+        Document document = parse(data);
+        extractNews(document);
+    }
 
-	private byte[] download() throws StatusException {
-		try {
-			return Downloader.download(url);
-		} catch (IOException ex) {
-			throw new StatusException(ResultStatus.CONNECTION_ERROR, "downloading", ex);
-		}
-	}
+    private byte[] download() throws StatusException {
+        try {
+            return Downloader.download(url);
+        } catch (IOException ex) {
+            throw new StatusException(ResultStatus.CONNECTION_ERROR, "downloading", ex);
+        }
+    }
 
-	private Document parse(byte[] data) throws StatusException {
-		try (ByteArrayInputStream input = new ByteArrayInputStream(data)) {
-			return Jsoup.parse(input, "UTF-8", url.toString());
-		} catch (Exception ex) {
-			throw new StatusException(ResultStatus.DATA_ERROR, "parsing", ex);
-		}
-	}
+    private Document parse(byte[] data) throws StatusException {
+        try (ByteArrayInputStream input = new ByteArrayInputStream(data)) {
+            return Jsoup.parse(input, "UTF-8", url.toString());
+        } catch (Exception ex) {
+            throw new StatusException(ResultStatus.DATA_ERROR, "parsing", ex);
+        }
+    }
 
-	private void extractNews(Document document) {
-	}
+    private void extractNews(Document document) {
+    }
 
 }
