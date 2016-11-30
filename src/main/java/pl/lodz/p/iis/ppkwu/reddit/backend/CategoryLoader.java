f82f43ef13758cf20cfd791dbbb9c82575d06c8d
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-30 19:27:38 +0100

    Poprawka CategoryLoadera

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryLoader.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryLoader.java
@@ -2,24 +2,27 @@ package pl.lodz.p.iis.ppkwu.reddit.backend;
 
 import static pl.lodz.p.iis.ppkwu.reddit.backend.utils.InvocationChecker.checkInvocation;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import pl.lodz.p.iis.ppkwu.reddit.api.Category;
-import pl.lodz.p.iis.ppkwu.reddit.api.ResultStatus;
 import pl.lodz.p.iis.ppkwu.reddit.backend.data.builders.CategoryBuilder;
 import pl.lodz.p.iis.ppkwu.reddit.backend.exceptions.StatusException;
 import pl.lodz.p.iis.ppkwu.reddit.backend.utils.ImmutableListBuilder;
 
 public class CategoryLoader extends AbstractLoader<List<Category>, ImmutableListBuilder<Category>> {
 
-	private static final String PROMOTED_CATEGORY = "promoted";
-	private static final String WIKI_CATEGORY = "wiki";
-	private static final String GILDED_CATEGORY = "gilded";
-	private static final String HOT_CATEGORY = "hot";
+	private static Set<String> NON_CATEGORY_URLS = new HashSet<>();
+
+	static {
+		NON_CATEGORY_URLS.add("wiki");
+		NON_CATEGORY_URLS.add("gilded");
+	}
 
 	protected CategoryLoader() {
 		super(UrlGenerator.baseUrl(), new ImmutableListBuilder<>());
@@ -32,69 +35,46 @@ public class CategoryLoader extends AbstractLoader<List<Category>, ImmutableList
 
 		if (null != categories) {
 			for (Element categoryElement : categories) {
-
 				CategoryBuilder categoryBuilder = new CategoryBuilder();
-				String url = getURL(categoryElement);
 
-				String categoryName = "";
-				boolean validCategory = true;
+				String name = getCategoryName(categoryElement);
+				categoryBuilder.withName(name);
 
-				String[] parts = url.split("//");
-				String[] subparts = parts[1].split("/");
-
-				int subpartsLength = subparts.length;
-
-				switch (subpartsLength) {
-				case 1:
-					categoryName = HOT_CATEGORY;
-					break;
-				case 2:
-					categoryName = subparts[subpartsLength - 1];
-					validCategory = isValidCategory(categoryName);
-					break;
+				String relativeUrl = getCategoryRelativeUrl(categoryElement);
+				if (NON_CATEGORY_URLS.contains(relativeUrl)) {
+					continue;
 				}
+				categoryBuilder.withRelativeUrl(relativeUrl);
 
-				if (subpartsLength > 2) {
-					if (subparts[3].isEmpty()) {
-						categoryName = HOT_CATEGORY;
-					} else {
-						categoryName = subparts[3];
-						validCategory = isValidCategory(categoryName);
-					}
-				}
-
-				if (validCategory) {
-					String relativeUrl = getRelativeUrl(subparts, subpartsLength);
-					categoryBuilder.withName(categoryName).withRelativeUrl(relativeUrl);
-					contentBuilder.addEntry(categoryBuilder.build());
-				}
+				contentBuilder.addEntry(categoryBuilder.build());
 			}
 		}
 	}
 
-	private String getRelativeUrl(String[] subparts, int subpartsLength) {
-		String relativeUrl = "/";
-		for (int i = 1; i < subpartsLength; i++) {
-			relativeUrl = relativeUrl + subparts[i] + "/";
+	private String getCategoryRelativeUrl(Element categoryElement) {
+		String absoluteUrl = categoryElement.absUrl("href");
+		int catIdx = indexOfNth(absoluteUrl, 3, '/');
+		String relativeUrl = absoluteUrl.substring(catIdx + 1);
+		if (relativeUrl.endsWith("/")) {
+			relativeUrl = relativeUrl.substring(0, relativeUrl.length() - 1);
 		}
 		return relativeUrl;
 	}
 
-	private boolean isValidCategory(String categoryName) {
-		return !GILDED_CATEGORY.equals(categoryName) && !WIKI_CATEGORY.equals(categoryName)
-				&& !PROMOTED_CATEGORY.equals(categoryName);
+	private int indexOfNth(String string, int n, char ch) {
+		int idx = 0;
+		for (int i = 0; i < n && idx != -1; ++i) {
+			idx = string.indexOf(ch, idx + 1);
+		}
+		return idx;
 	}
 
-	private String getURL(Element categoryElement) throws StatusException {
-		Elements categoryElements = categoryElement.select("a.[href]");
-		if (categoryElements.isEmpty()) {
-			throw new StatusException(ResultStatus.DATA_ERROR, "no category");
-		}
-		return categoryElements.first().text();
+	private String getCategoryName(Element categoryElement) {
+		return categoryElement.text();
 	}
 
 	private Elements getCategoriesFromDocument(Document document) {
-		return document.select("ul.tabmenu, a.choice");
+		return document.select("ul.tabmenu a.choice");
 	}
 
 }
