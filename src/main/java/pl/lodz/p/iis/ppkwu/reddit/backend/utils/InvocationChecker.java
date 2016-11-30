7d1eaf277b6eb33c4d6339e642cc84f05c5b92cf
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-30 03:32:50 +0100

    wstepna implementacja checkInvocation

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/utils/InvocationChecker.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/utils/InvocationChecker.java
@@ -2,11 +2,33 @@ package pl.lodz.p.iis.ppkwu.reddit.backend.utils;
 
 public class InvocationChecker {
 
+	private static String[] PERMITTED_PREFIXES = { "pl.lodz.p.iis.ppkwu.reddit.backend.",
+			"pl.lodz.p.iis.ppkwu.reddit.api." };
+
+	private static int CALLER_OF_CALLER_OF_CHECKER = 3;
+
 	private InvocationChecker() {
 		throw new UnsupportedOperationException();
 	}
 
 	public static void checkInvocation() {
+		StackTraceElement[] stackTrace = getStackTrace();
+		StackTraceElement callerOfCaller = stackTrace[CALLER_OF_CALLER_OF_CHECKER];
+		String className = callerOfCaller.getClassName();
+		System.out.println(className);
+		for (String prefix : PERMITTED_PREFIXES) {
+			if (className.startsWith(prefix)) {
+				return;
+			}
+		}
+	}
+
+	private static StackTraceElement[] getStackTrace() {
+		try {
+			throw new Throwable();
+		} catch (Throwable thr) {
+			return thr.getStackTrace();
+		}
 	}
 
 }
