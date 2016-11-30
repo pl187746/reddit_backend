3f1d5a52a485f68afb71fd7db787a966fe29f035
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-30 03:06:03 +0100

    stub metody checkInvokation

---
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/utils/InvocationChecker.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.iis.ppkwu.reddit.backend.utils;
+
+public class InvocationChecker {
+
+	private InvocationChecker() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static void checkInvocation() {
+	}
+
+}
