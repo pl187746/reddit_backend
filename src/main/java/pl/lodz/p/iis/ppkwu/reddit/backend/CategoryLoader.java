29bfae0786730539070e189ee531cc2e2a652725
Karol Kucharski <187746@edu.p.lodz.pl>
2016-12-01 22:00:21 +0100

    dokladniejsze sprawdzanie url kategorii

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryLoader.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryLoader.java
@@ -11,6 +11,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import pl.lodz.p.iis.ppkwu.reddit.api.Category;
+import pl.lodz.p.iis.ppkwu.reddit.api.ResultStatus;
 import pl.lodz.p.iis.ppkwu.reddit.backend.data.builders.CategoryBuilder;
 import pl.lodz.p.iis.ppkwu.reddit.backend.exceptions.StatusException;
 import pl.lodz.p.iis.ppkwu.reddit.backend.utils.ImmutableListBuilder;
@@ -33,27 +34,36 @@ public class CategoryLoader extends AbstractLoader<List<Category>, ImmutableList
 	protected void extract(Document document) throws StatusException {
 		Elements categories = getCategoriesFromDocument(document);
 
-		if (null != categories) {
-			for (Element categoryElement : categories) {
-				CategoryBuilder categoryBuilder = new CategoryBuilder();
-
-				String name = getCategoryName(categoryElement);
-				categoryBuilder.withName(name);
-
-				String relativeUrl = getCategoryRelativeUrl(categoryElement);
-				if (NON_CATEGORY_URLS.contains(relativeUrl)) {
-					continue;
-				}
-				categoryBuilder.withRelativeUrl(relativeUrl);
-
-				contentBuilder.addEntry(categoryBuilder.build());
-			}
+		if (categories == null || categories.isEmpty()) {
+			throw new StatusException(ResultStatus.DATA_ERROR, "no categories");
 		}
+
+		for (Element categoryElement : categories) {
+			CategoryBuilder categoryBuilder = new CategoryBuilder();
+
+			String name = getCategoryName(categoryElement);
+			categoryBuilder.withName(name);
+
+			String relativeUrl = getCategoryRelativeUrl(categoryElement);
+			if (invalidCategoryReltiveUrl(relativeUrl)) {
+				continue;
+			}
+			categoryBuilder.withRelativeUrl(relativeUrl);
+
+			contentBuilder.addEntry(categoryBuilder.build());
+		}
+	}
+
+	private boolean invalidCategoryReltiveUrl(String relativeUrl) {
+		return relativeUrl == null || NON_CATEGORY_URLS.contains(relativeUrl);
 	}
 
 	private String getCategoryRelativeUrl(Element categoryElement) {
 		String absoluteUrl = categoryElement.absUrl("href");
 		int catIdx = indexOfNth(absoluteUrl, 3, '/');
+		if (catIdx == -1) {
+			return null;
+		}
 		String relativeUrl = absoluteUrl.substring(catIdx + 1);
 		if (relativeUrl.endsWith("/")) {
 			relativeUrl = relativeUrl.substring(0, relativeUrl.length() - 1);
