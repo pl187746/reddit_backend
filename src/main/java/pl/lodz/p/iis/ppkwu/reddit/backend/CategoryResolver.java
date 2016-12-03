3d7c54577a54fa1701b4cfebdb07efcfed9c6677
Karol Kucharski <187746@edu.p.lodz.pl>
2016-12-04 00:30:51 +0100

    metoda resolve

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryResolver.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryResolver.java
@@ -1,9 +1,19 @@
 package pl.lodz.p.iis.ppkwu.reddit.backend;
 
+import java.util.Objects;
+
+import pl.lodz.p.iis.ppkwu.reddit.api.Category;
+import pl.lodz.p.iis.ppkwu.reddit.backend.data.CategoryImpl;
+
 public class CategoryResolver {
 
 	private CategoryResolver() {
 		throw new UnsupportedOperationException();
+	}
+
+	public static CategoryImpl resolve(Category category) {
+		Objects.requireNonNull(category);
+		return (CategoryImpl) category;
 	}
 
 }
