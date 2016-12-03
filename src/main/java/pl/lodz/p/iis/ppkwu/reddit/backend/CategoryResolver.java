b0ca5c2f417c03c7b117f72ff9ef8ff8f26c605d
Karol Kucharski <187746@edu.p.lodz.pl>
2016-12-04 00:47:30 +0100

    Wyszukiwanie kategorii po nazwie.

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryResolver.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/backend/CategoryResolver.java
@@ -1,9 +1,15 @@
 package pl.lodz.p.iis.ppkwu.reddit.backend;
 
+import static pl.lodz.p.iis.ppkwu.reddit.backend.utils.InvocationChecker.checkInvocation;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 import pl.lodz.p.iis.ppkwu.reddit.api.Category;
+import pl.lodz.p.iis.ppkwu.reddit.api.Result;
 import pl.lodz.p.iis.ppkwu.reddit.backend.data.CategoryImpl;
+import pl.lodz.p.iis.ppkwu.reddit.backend.exceptions.InvalidNameException;
 
 public class CategoryResolver {
 
@@ -11,9 +17,45 @@ public class CategoryResolver {
 		throw new UnsupportedOperationException();
 	}
 
-	public static CategoryImpl resolve(Category category) {
+	public static CategoryImpl resolve(Category category) throws InvalidNameException {
 		Objects.requireNonNull(category);
+		checkInvocation();
+		if (category instanceof CategoryImpl) {
+			return (CategoryImpl) category;
+		} else {
+			return findByName(category.name());
+		}
+	}
+
+	public static CategoryImpl findByName(String name) throws InvalidNameException {
+		Objects.requireNonNull(name);
+		checkInvocation();
+		List<Category> list = getCategoriesList();
+		Category category = getByName(list, name);
+		return asImpl(category);
+	}
+
+	private static CategoryImpl asImpl(Category category) {
 		return (CategoryImpl) category;
+	}
+
+	private static Category getByName(List<Category> list, String name) throws InvalidNameException {
+		for (Category category : list) {
+			if (name.equalsIgnoreCase(category.name())) {
+				return category;
+			}
+		}
+		throw new InvalidNameException("category with name '" + name + "' not found");
+	}
+
+	private static List<Category> getCategoriesList() {
+		Result<List<Category>> result = loadCategoriesList();
+		return result.content().orElse(Collections.emptyList());
+	}
+
+	private static Result<List<Category>> loadCategoriesList() {
+		CategoryLoader categoryLoader = new CategoryLoader();
+		return categoryLoader.load();
 	}
 
 }
